175d59ae048df2a584b552ec3929f336b8f54b1d
miguelnery <nery.mik@gmail.com>
2023-11-21 10:19:08 -0300

    Initial Commit

---
+++ VidenteCaindo/VidenteCaindoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VidenteCaindoApp.swift
+//  VidenteCaindo
+//
+//  Created by mik on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VidenteCaindoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
